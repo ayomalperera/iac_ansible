1040777abc35457ef056da1bf54f1fce863d2e67
root <root@vmi3086623.contaboserver.net>
2026-05-15 21:58:38 +0530

    a

--- terraform/airarabia/main.tf
+++ terraform/airarabia/main.tf
@@ -258,7 +258,7 @@ resource "aws_instance" "haproxy" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.haproxy_sg.id]
   associate_public_ip_address = true
-  key_name = aws_key_pair.airarabia.key_name
+  key_name                    = aws_key_pair.airarabia.key_name
 
   tags = {
     Name        = "${var.project_name}-haproxy"
@@ -291,7 +291,7 @@ resource "aws_instance" "app_servers" {
   instance_type          = var.instance_type
   subnet_id              = each.value
   vpc_security_group_ids = [aws_security_group.app_sg.id]
-  key_name = aws_key_pair.airarabia.key_name
+  key_name               = aws_key_pair.airarabia.key_name
 
   tags = {
     Name        = "${var.project_name}-${each.key}"
